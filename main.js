f92bea7eb757b3c0f2910f8f2657817cd56ffe37
vinicius rodrigues <101264810+15vini15@users.noreply.github.com>
2022-04-27 14:18:19 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+class MobileNavbar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobilemenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll (navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind();
+
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index) =>{
+            
+            link.style.animation
+            ? (link.style.animation = "")
+            :(link.style.animation = 'navLinkFade 0.5s ease forwards ${ index / 7 + 0.3} s');
+        });
+    }
+
+    handleClick(){
+    this.navlist.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animatedLink();
+    }
+
+    addClickEvent(){
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent
+        }
+        return this;
+    }
+}
+
